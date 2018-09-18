cd473c9b70015b90d6883210f211d90395c55f8b
todesking <discommunicative@gmail.com>
2018-09-18 19:05:47 +0900

    Checkpoint: cache(args)(f)

--- core/src/main/scala/cache/Checkpoint.scala
+++ core/src/main/scala/cache/Checkpoint.scala
@@ -5,7 +5,8 @@ import scala.reflect.macros.blackbox.Context
 import scala.language.experimental.macros
 
 class Checkpoint(val fs: CacheFS) {
-  def nocache[R](f: R): Dep[R] = macro Checkpoint.NoCacheImpl.apply[R]
+  def nocache[R](f: R): Dep[R] = macro Checkpoint.NoCacheImpl.apply0[R]
+  def nocache[A, R](args: DepArg[A])(f: A => R): Dep[R] = macro Checkpoint.NoCacheImpl.apply1[A, R]
   def cache0[R: Cacheable](f: R): Dep[R] = macro Checkpoint.CacheImpl.apply0[R]
   def cache[A, R: Cacheable](args: DepArg[A])(f: A => R): Dep[R] = macro Checkpoint.CacheImpl.apply1[A, R]
 
@@ -25,12 +26,18 @@ object Checkpoint {
     import c.Expr
     import c.WeakTypeTag
     import c.universe.Quasiquote
+    import c.universe.Tree
 
-    def apply[R: WeakTypeTag](f: Expr[R]): Expr[Dep[R]] = {
+    def apply0[R: WeakTypeTag](f: Expr[R]): Expr[Dep[R]] =
+      impl[Unit, R](f.tree.toString, f.tree)
+
+    def apply1[A: WeakTypeTag, R: WeakTypeTag](args: Expr[DepArg[A]])(f: Expr[R]): Expr[Dep[R]] =
+      impl[A, R](f.tree.toString, q"$f($args.value)")
+
+    private[this] def impl[A: WeakTypeTag, R: WeakTypeTag](src: String, value: Tree): Expr[Dep[R]] = {
       val name = c.internal.enclosingOwner.name.decodedName.toString
-      def src = f.tree.toString
       val id = q"_root_.com.todesking.scalanb.cache.DepID($name, $src, _root_.scala.collection.immutable.Seq())"
-      Expr[Dep[R]](q"_root_.com.todesking.scalanb.cache.Dep.buildUNSAFE($id, $f)")
+      Expr[Dep[R]](q"_root_.com.todesking.scalanb.cache.Dep.buildUNSAFE($id, $value)")
     }
   }
   class CacheImpl(val c: Context) {
