a80c0d79bd7933904d4dbef1afdb9d8126f58a6a
todesking <discommunicative@gmail.com>
2018-10-04 21:56:14 +0900

    Cache: lazy nocache(take 2)

--- core/src/main/scala/cache/DepArg.scala
+++ core/src/main/scala/cache/DepArg.scala
@@ -4,7 +4,9 @@ import com.todesking.scalanb.impl
 
 import scala.language.implicitConversions
 
-class DepArg[A](val ids: Seq[DepID], val value: A)
+class DepArg[A](val ids: Seq[DepID], val calc: => A) {
+  lazy val value: A = calc
+}
 object DepArg extends impl.cache.DepArgTupleInstances {
   implicit def fromDep[A, B](x: A)(implicit ev: Dependable[A, B]): DepArg[B] = {
     val dep = ev(x)
