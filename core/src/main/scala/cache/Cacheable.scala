d9fea0c09ed40f4d275cca796fe10eea699dbb08
todesking <discommunicative@gmail.com>
2019-06-26 23:51:12 +0900

    refactor

--- core/src/main/scala/cache/Cacheable.scala
+++ core/src/main/scala/cache/Cacheable.scala
@@ -42,22 +42,24 @@ object Cacheable extends CacheableLowPriority {
 
   def of[A: Cacheable]: Cacheable[A] = implicitly[Cacheable[A]]
 
-  def byJavaSerialization[A](doPut: (A, ObjectOutputStream) => Unit)(doGet: ObjectInputStream => A): Cacheable[A] = new Cacheable[A] {
-    override def save(fs: FileSystem, name: String)(value: A) = {
+  def apply[A](ser: (FileSystem, String, A) => Unit)(de: (FileSystem, String) => A): Cacheable[A] = new Cacheable[A] {
+    override def save(fs: FileSystem, name: String)(value: A) = ser(fs, name, value)
+    override def load(fs: FileSystem, name: String) = de(fs, name)
+  }
+
+  def byJavaSerialization[A](doPut: (A, ObjectOutputStream) => Unit)(doGet: ObjectInputStream => A): Cacheable[A] =
+    Cacheable[A] { (fs, name, value) =>
       val buf = new java.io.ByteArrayOutputStream()
       val os = new java.io.ObjectOutputStream(buf)
       doPut(value, os)
       os.close()
       val data = buf.toByteArray
       fs.writeBytes(name, data)
-    }
-
-    override def load(fs: FileSystem, name: String) = {
+    } { (fs, name) =>
       val data = fs.readBytes(name)
       val is = new java.io.ObjectInputStream(new java.io.ByteArrayInputStream(data))
       doGet(is)
     }
-  }
 
   // This method should not be implicit:
   // Some Serializables are not suitable for caching(e.g. Spark DataFrame)
